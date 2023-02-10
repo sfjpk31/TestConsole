beacf72971872ea79024a9c559c62fbc81e37b95
Joseph Kelly <jkelly@opentable.com>
2023-02-09 16:09:06 -0800

    Added files

---
+++ TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TRServiceClient
+{
+    using System;
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestService();
+
+            Console.WriteLine("Th-th-that's all, folks!");
+            Console.ReadLine();
+        }
+
+        private static void TestService()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
